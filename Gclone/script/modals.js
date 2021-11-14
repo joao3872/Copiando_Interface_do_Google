6764c36dff710c8c5d0057ea62389ea42c5d8415
joao3872 <68516158+joao3872@users.noreply.github.com>
2021-11-14 02:52:38 -0300

    Update modals.js

--- Gclone/script/modals.js
+++ Gclone/script/modals.js
@@ -1,43 +1,43 @@
 function abrirNav() {
-				document.querySelector("section#side_Nav").style.width = '100%'
+    document.querySelector("section#side_Nav").style.width = '100%'
 }
 
 function fecharNav() {
-				document.querySelector("section#side_Nav").style.width = '0%'
+    document.querySelector("section#side_Nav").style.width = '0%'
 }
 
 
 function abrirPesquisa() {
-				document.querySelector("div#pesquisa").style.display = 'block'
+    document.querySelector("div#pesquisa").style.display = 'block'
 }
 
 function fecharPesquisa() {
-				document.querySelector("div#pesquisa").style.display = 'none'
+    document.querySelector("div#pesquisa").style.display = 'none'
 }
 
 
 function abrirAudio() {
-				document.querySelector("section#audio").style.display = 'block'
+    document.querySelector("section#audio").style.display = 'block'
 }
 
 function fecharAudio() {
-				document.querySelector("section#audio").style.display = 'none'
+    document.querySelector("section#audio").style.display = 'none'
 }
 
 
 function alterarFoto() {
-				document.querySelector("div#trocar_foto").style.display = 'block'
+    document.querySelector("div#trocar_foto").style.display = 'block'
 }
 
 function fecharAlterarFoto() {
-				document.querySelector("div#trocar_foto").style.display = 'none'
+    document.querySelector("div#trocar_foto").style.display = 'none'
 }
 
 
 function abrirFeedback() {
-				document.querySelector("div#feedback").style.display = 'block'
+    document.querySelector("div#feedback").style.display = 'block'
 }
 
 function fecharFeedback() {
-				document.querySelector("div#feedback").style.display = 'none'
+    document.querySelector("div#feedback").style.display = 'none'
 }
