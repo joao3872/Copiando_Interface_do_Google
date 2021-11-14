0f218bd5d201d5b502abd9889a1cace2f90d0061
joao3872 <68516158+joao3872@users.noreply.github.com>
2021-11-14 02:44:25 -0300

    Add files via upload

---
+++ Gclone/script/modals.js
@@ -0,0 +1,43 @@
+function abrirNav() {
+				document.querySelector("section#side_Nav").style.width = '100%'
+}
+
+function fecharNav() {
+				document.querySelector("section#side_Nav").style.width = '0%'
+}
+
+
+function abrirPesquisa() {
+				document.querySelector("div#pesquisa").style.display = 'block'
+}
+
+function fecharPesquisa() {
+				document.querySelector("div#pesquisa").style.display = 'none'
+}
+
+
+function abrirAudio() {
+				document.querySelector("section#audio").style.display = 'block'
+}
+
+function fecharAudio() {
+				document.querySelector("section#audio").style.display = 'none'
+}
+
+
+function alterarFoto() {
+				document.querySelector("div#trocar_foto").style.display = 'block'
+}
+
+function fecharAlterarFoto() {
+				document.querySelector("div#trocar_foto").style.display = 'none'
+}
+
+
+function abrirFeedback() {
+				document.querySelector("div#feedback").style.display = 'block'
+}
+
+function fecharFeedback() {
+				document.querySelector("div#feedback").style.display = 'none'
+}
